8ca888f01bd4d4a620059782edf106f117c01f0d
Thanasis Politis <thanasis.politis@miraclemill.com>
2023-03-25 23:17:45 +0200

    Revert code to the main branch version

--- main.go
+++ main.go
@@ -39,13 +39,5 @@ func main() {
 		fmt.Fprintf(w, "Hello, %s!\n", reversed)
 	})
 
-	http.HandleFunc("/reverse_2", func(w http.ResponseWriter, r *http.Request) {
-		reversed, err := reverser.Reverse(r.Context(), r.URL.Query().Get("name"))
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-		}
-		fmt.Fprintf(w, "Hello, %s!\n", reversed)
-	})
-
 	http.Serve(lis, nil)
 }
